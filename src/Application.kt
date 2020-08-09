0450a340d7c458bbeb24cd9a0878d8e771f8896d
sin <ogasawaraShinnosuke@users.noreply.github.com>
2020-08-09 18:25:37 +0900

    Update Application.kt

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,7 @@
 package com.a_pags_server
 
+import com.a_pags_server.application.service.UserService
+import com.a_pags_server.application.service.serviceModule
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
 import io.ktor.response.*
@@ -8,7 +10,8 @@ import io.ktor.jackson.jackson
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
-import java.time.LocalDateTime
+import org.koin.ktor.ext.Koin
+import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -20,15 +23,20 @@ fun Application.module(testing: Boolean = false) {
             configure(SerializationFeature.INDENT_OUTPUT, true)
         }
     }
+    install(Koin) {
+        modules(serviceModule)
+    }
+    val userService by inject<UserService>()
     routing {
         route("/api/v1/users") {
+
             get {
-                val users = createUsers()
+                val users = userService.getUsers()
                 call.respond(users)
             }
             get("/{id}") {
                 val id: Int = Integer.parseInt(call.parameters["id"])
-                val user = createUser(id)
+                val user = userService.getUser(id)
                 call.respond(user)
             }
         }
@@ -36,33 +44,4 @@ fun Application.module(testing: Boolean = false) {
             call.respondText("Hello, World")
         }
    }
-}
-
-data class User(
-    val id: Int,
-    val fullName: String,
-    val hometown: String,
-    val residence: String,
-    val education: String,
-    val job: String,
-    val created_at: LocalDateTime,
-    val updated_at: LocalDateTime
-)
-
-fun sampleData(): User {
-    return User(1, "ogasawaraShinnosuke", "SECRET", "東京", "情報システム修士", "エンジニア",
-        LocalDateTime.of(2020, 8, 3, 16, 45, 51),
-        LocalDateTime.of(2020, 8, 5, 16, 45, 51)
-    )
-}
-
-fun createUser(id: Int): User {
-    val user = sampleData()
-    if (id == user.id) {
-        return user
-    }
-    return User(0, "NO NAME", "NO TOWN", "NO TOWN", "NO", "NO JOB", LocalDateTime.now(), LocalDateTime.now())
-}
-fun createUsers(): List<User> {
-    return listOf(sampleData())
 }
