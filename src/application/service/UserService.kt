91d88959e45cf2b001e6744cde9d56f78147bcb7
sin <ogasawaraShinnosuke@users.noreply.github.com>
2020-08-10 13:14:33 +0900

    Update UserService.kt

--- src/application/service/UserService.kt
+++ src/application/service/UserService.kt
@@ -1,24 +1,26 @@
 package com.a_pags_server.application.service
 
+import com.a_pags_server.domain.model.Dao
 import com.a_pags_server.domain.model.UserDTO
-import com.a_pags_server.domain.model.UserDao
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
 interface UserService {
 
     fun getUsers(): List<UserDTO>
 
-    fun getUser(id: Int): UserDTO
+    fun getUser(id: Long): UserDTO
 }
 
-class UserServiceImpl: UserService {
+class UserServiceImpl: UserService, KoinComponent {
+
+    private val dao by inject<Dao<UserDTO>>()
 
     override fun getUsers(): List<UserDTO> {
-        val userDao = UserDao()
-        return userDao.findAll()
+        return dao.findAll()
     }
 
-    override fun getUser(id: Int): UserDTO {
-        val userDao = UserDao()
-        return userDao.findById(id)
+    override fun getUser(id: Long): UserDTO {
+        return dao.findById(id)
     }
 }
